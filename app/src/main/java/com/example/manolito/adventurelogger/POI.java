d0a02b4ec28f4e3ebbd0b09f322656756d00a72d
geoffshaw <geoffshawbc@gmail.com>
2016-04-05 11:50:31 -0700

    Plots trips properly

---
+++ app/src/main/java/com/example/manolito/adventurelogger/POI.java
@@ -0,0 +1,13 @@
+package com.example.manolito.adventurelogger;
+
+/**
+ * Created by geoffshaw on 16-04-03.
+ */
+public class POI {
+    double latitude, longitude;
+
+    public POI(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ app/src/main/java/com/example/manolito/adventurelogger/TripStatistics.java
@@ -0,0 +1,29 @@
+package com.example.manolito.adventurelogger;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TripStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_statistics);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+}
